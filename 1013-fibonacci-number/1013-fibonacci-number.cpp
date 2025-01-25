7954c865957b22a6cc53036b00357eb88db27baa
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-01-25 21:21:45 +0530

    Time: 13 ms (31.58%), Space: 7.6 MB (85.76%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
+int find(int n){
+    if(n<=1)
+    return n;
+    return find(n-1)+find(n-2);
+}
     int fib(int n) {
         // base case
-        if(n==0)
-        return 0;
-        if(n==1)
-        return 1;
-        int ans=fib(n-1)+fib(n-2);
-        return ans;
+        return find(n);
+ 
     }
 };
