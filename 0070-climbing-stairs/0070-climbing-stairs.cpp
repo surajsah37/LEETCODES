4d322d042e3e45408731678aeb73ce6e21f888f9
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-01-25 22:38:17 +0530

    Time: 0 ms (100%), Space: 8.5 MB (34.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,6 +7,8 @@ public:
     }
 };
 */
+// using recursion and top up arrpoach
+/*
 class Solution {
 public:
 int step(int stair,int n, vector<int>&dp){
@@ -26,5 +28,33 @@ int step(int stair,int n, vector<int>&dp){
         vector<int>dp(n+2,-1);
         dp[n]=1,dp[n+1]=0;
        return step(0,n,dp);
+    }
+};
+
+*/
+
+// using button approach in the dp
+class Solution {
+public:
+int step(int stair,int n, vector<int>&dp){
+    if(stair==n)
+    return 1;
+    if(stair>n)
+    return 0;
+
+    // already calculated case
+    if(dp[stair]!=-1)
+    return dp[stair];
+
+    dp[stair]=step(stair+1,n,dp)+step(stair+2,n,dp);
+    return dp[stair];
+}
+    int climbStairs(int n) {
+        vector<int>dp(n+2,-1);
+       dp[n]=1;
+       dp[n+1]=0;
+       for(int i=n-1;i>=0;i--)
+       dp[i]=dp[i+1]+dp[i+2];
+       return dp[0];
     }
 };
