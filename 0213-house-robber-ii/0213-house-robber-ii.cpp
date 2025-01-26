fe4577ad437fbb28804c2921fdb655f194c81cbf
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-01-26 14:59:17 +0530

    Time: 3 ms (7.89%), Space: 10.9 MB (41.17%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -12,8 +12,8 @@ int find(int index,int n,vector<int>& nums)
     }
 };
 */
-
-// converting into the dp
+/*
+// converting into the dp top down approach
 class Solution {
 public:
 int find(int index,int n,vector<int>& nums, vector<int>&dp)
@@ -27,14 +27,33 @@ int find(int index,int n,vector<int>& nums, vector<int>&dp)
 }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)
+        if(n==1)// when only one house 
         return nums[0];
         vector<int>dp1(n+2,-1);
         vector<int>dp2(n+2,-1);
         return max(find(0,n-1,nums,dp1),find(1,n,nums,dp2));
     }
 };
+*/
+// converting into the   down to top approach
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1)// when only one house 
+        return nums[0];
+        vector<int>dp1(n+2,-1);
+        vector<int>dp2(n+2,-1);
+        dp1[n-1]=dp1[n]=dp2[n]=dp2[n+1]=0;
 
+        for(int i=n-2;i>-1;i--)
+        dp1[i]=max(nums[i]+dp1[i+2],dp1[i+1]);
+
+        for(int i=n-1;i>0;i--)
+        dp2[i]=max(nums[i]+dp2[i+2],dp2[i+1]);
+        return max(dp1[0],dp2[1]);
+    }
+};
 
 
 
