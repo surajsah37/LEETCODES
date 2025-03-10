9d0630b31865071be24f417e9cf1efc34e5a5d3c
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-03-10 23:19:54 +0530

    Time: 0 ms (100%), Space: 8.8 MB (81.14%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+         int count = 0;   // Tracks the depth of nested parentheses
+        string ans;      // Stores the final modified string
+
+        // Iterate through the given string
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ')') count--;  // Decrement count when encountering ')'
+
+            if (count != 0)            // If count is not zero, append to result (to skip outermost parentheses)
+                ans.push_back(s[i]);   // Append current character to the result
+
+            if (s[i] == '(') count++;  // Increment count when encountering '('
+        }
+
+        return ans;  // Return the modified string without outermost parentheses
+    }
+};
