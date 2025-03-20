5ce7aa0e23c4f58224c3da9fe33da36998732589
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-03-20 20:51:45 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.26%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
+        int n = nums.size(); // Get the size of the array
         int i, j;
 
         // Step 1: Find the first decreasing element from the right
         for (i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
+            if (nums[i] < nums[i + 1]) {   // Identify the pivot where the order breaks
                 break;
             }
         }
 
         // Step 2: If such an element is found, find the element just larger than it
         if (i >= 0) {
-            for (j = n - 1; j > i; j--) {
-                if (nums[j] > nums[i]) {
+            for (j = n - 1; j > i; j--) { // Start searching from the rightmost side
+                if (nums[j] > nums[i]) { // Find the next greater element
                     break;
                 }
             }
