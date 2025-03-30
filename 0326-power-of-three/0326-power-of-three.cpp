f69dfaee01b74357840d015356c0810538b2b4bc
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-03-30 11:45:37 +0530

    Time: 4 ms (48.68%), Space: 8.9 MB (78.11%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        /*
-        if(n<=0)  return false;
-        long long x=1;
-        while(x<=n){
-            if(x==1)   return true;
-            x*=3;
-        }
-        return false;
-*/
-// The largest power of 3 within the range of int (3^19 = 1162261467)
-    //return (n > 0 && 1162261467 % n == 0);
     if(n<1)
     return false;
     while(n%3==0)
@@ -19,3 +8,6 @@ public:
     return n==1;
     }
 };
+
+// The largest power of 3 within the range of int (3^19 = 1162261467)
+    //return (n > 0 && 1162261467 % n == 0);
