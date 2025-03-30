4f5ae443cc0f17ab4b91028a5533f8c084a75209
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-03-30 11:43:17 +0530

    Time: 1 ms (74.35%), Space: 9.1 MB (15.48%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -11,6 +11,11 @@ public:
         return false;
 */
 // The largest power of 3 within the range of int (3^19 = 1162261467)
-    return (n > 0 && 1162261467 % n == 0);
+    //return (n > 0 && 1162261467 % n == 0);
+    if(n<1)
+    return false;
+    while(n%3==0)
+    n/=3;
+    return n==1;
     }
 };
