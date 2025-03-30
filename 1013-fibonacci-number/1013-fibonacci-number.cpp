2d1174b8294fd19b6ac9f717665b7f2492a887f8
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-03-30 21:05:31 +0530

    Time: 3 ms (66.75%), Space: 8.3 MB (10.29%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
 int find(int n){
     if(n<=1)
     return n;
@@ -8,6 +9,25 @@ int find(int n){
     int fib(int n) {
         // base case
         return find(n);
+ 
+    }
+    */
+
+    int find(int n,vector<int>&dp){
+    if(n<=1)
+    return n;
+
+    if(dp[n]!=-1)
+    return dp[n];
+
+    dp[n]= find(n-1,dp)+find(n-2,dp);
+    return dp[n];
+}
+    int fib(int n) {
+        // base case
+        vector<int>dp(n+1,-1);
+
+        return find(n,dp);
  
     }
 };
