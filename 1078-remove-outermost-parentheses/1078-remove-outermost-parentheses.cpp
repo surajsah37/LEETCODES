a692d9ddc3afdad51c7dfb69c34139075e2f43be
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-04-21 20:54:04 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.4%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
+       int count=0;
+       string ans;
+       for(int i=0;i<s.size();i++){
+
+        if(s[i]==')')  
+        count--;
+
+        if(count!=0)
+        ans.push_back(s[i]);
+
+        if(s[i]=='(')
+        count++;
+       }
+       return ans;
+    }
+};
+
+
+
+ /*
          int count = 0;   // Tracks the depth of nested parentheses
         string ans;      // Stores the final modified string
 
@@ -16,4 +36,4 @@ public:
 
         return ans;  // Return the modified string without outermost parentheses
     }
-};
+    */
