9db0e99923f4327a8682df4a26be2df9cd700285
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-04-29 11:08:38 +0530

    Time: 0 ms (100%), Space: 28.2 MB (24.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        /*
          int count = 0;      // Count of the current candidate
     int candidate = 0;  // Variable to store the majority candidate
 
@@ -14,5 +15,23 @@ public:
 
     return candidate; // The majority element is guaranteed to exist
 
+    }
+    */
+ int count = 0;             // Initialize the counter
+        int candidate = 0;         // Variable to store the potential majority element
+
+        for (int i = 0; i < nums.size(); i++) {   // Traverse the entire array
+            if (count == 0) {                     // If count is zero, choose new candidate
+                candidate = nums[i];
+            }
+
+            if (nums[i] == candidate) {           // If current element equals candidate
+                count++;                          // Increment count
+            } else {
+                count--;                          // Else, decrement count
+            }
+        }
+
+        return candidate;                         // Return the candidate as the majority element
     }
 };
