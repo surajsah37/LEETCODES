177800dbd9a4697c78a307dfcb0a4f458d89ad56
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-04-29 10:48:58 +0530

    Time: 0 ms (100%), Space: 12.4 MB (38.97%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        
         int i = m - 1;         // Pointer for the last element in nums1 (excluding extra space)
     int j = n - 1;         // Pointer for the last element in nums2
     int k = m + n - 1;     // Pointer for the last position in nums1
@@ -19,4 +20,5 @@ public:
        }
     
     }
+
 };
