c8a117fc97be37bd6fb2de5d13ebb89328969ff5
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-05-03 12:01:14 +0530

    Time: 2 ms (37.12%), Space: 17.8 MB (62.12%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -5,7 +5,7 @@ public:
              // Check if the current digit is odd (1, 3, 5, 7, 9)
             if((num[i]-'0')%2==1)
             {   
-                 // If it's odd, return the substring from beginning to this position (inclusive)
+ // If it's odd, return the substring from beginning to this position (inclusive)
                 return num.substr(0,i+1);
             }
         }
