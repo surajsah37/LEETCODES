bbf9eacdc3ed765738795ac33890e748311b1546
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-05-06 00:24:12 +0530

    Time: 1609 ms (38.14%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,4 +1,11 @@
+-- SELECT p.product_name,s.year,s.price
+-- FROM sales s
+-- JOIN product p
+-- ON s.product_id=p.product_id
+
+
+
 SELECT p.product_name,s.year,s.price
 FROM sales s
-JOIN product p
+LEFT JOIN product p
 ON s.product_id=p.product_id
