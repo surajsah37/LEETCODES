c02002c7e6472f095a3abe56a388377cfeaf1a63
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-06-21 14:08:18 +0530

    Time: 0 ms (100%), Space: 112.2 MB (93.97%) - LeetHub

--- 0134-gas-station/0134-gas-station.cpp
+++ 0134-gas-station/0134-gas-station.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-          int totalTank = 0, currTank = 0, startIndex = 0;
-    
-    for (int i = 0; i < gas.size(); ++i) {
-        int diff = gas[i] - cost[i];
-        totalTank += diff;
-        currTank += diff;
+        // Initialize total gas and total cost to 0
+        int totalGas = 0, totalCost = 0;
 
-        if (currTank < 0) {
-            // Can't reach next station, restart from i+1
-            startIndex = i + 1;
-            currTank = 0;
+        // 'tank' keeps track of gas in car during the journey
+        // 'start' is the index of the gas station where we start
+        int tank = 0, start = 0;
+
+        // Loop through all gas stations
+        for (int i = 0; i < gas.size(); i++) {
+            // Add current station's gas to totalGas
+            totalGas += gas[i];
+
+            // Add current station's cost to totalCost
+            totalCost += cost[i];
+
+            // Add net gain/loss of gas at this station to the tank
+            tank += gas[i] - cost[i];
+
+            // If tank is negative, it means we can't reach the next station
+            if (tank < 0) {
+                // So, set the next station as a new starting point
+                start = i + 1;
+
+                // Reset tank to 0 since we're starting fresh
+                tank = 0;
+            }
         }
-    }
-    
-    return totalTank >= 0 ? startIndex : -1;
+
+        // If total gas is less than total cost, return -1 (not possible to complete circuit)
+        // Otherwise, return the starting index
+        return (totalGas < totalCost) ? -1 : start;
     }
 };
