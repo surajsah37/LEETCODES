f4025b6c4792f30849b69d87d7bef5d5708a6e19
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-06-27 10:18:43 +0530

    Time: 0 ms (100%), Space: 9.7 MB (93.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,36 +1,7 @@
 class Solution {
 public:
-
     bool isPalindrome(string s) {
-//        int left = 0, right = s.length() - 1;  // Two pointers at start and end
-
-//     while (left < right) {
-//         // Move left pointer forward if it's not an alphanumeric character
-//         while (left < right && !isalnum(s[left])) {
-//             left++;
-//         }
-        
-//         // Move right pointer backward if it's not an alphanumeric character
-//         while (left < right && !isalnum(s[right])) {
-//             right--;
-//         }
-
-//         // Convert both characters to lowercase and compare
-//         if (tolower(s[left]) != tolower(s[right])) {
-//             return false;  // If mismatch, it's not a palindrome
-//         }
-
-//         left++;   // Move left pointer forward
-//         right--;  // Move right pointer backward
-//     }
-
-//     return true;  // If all characters match, it's a palindrome
-// }
-
-
-
-int left = 0, right = s.size() - 1;
-
+     int left = 0, right = s.size() - 1;
     while (left < right) {
         // Move left pointer to the next alphanumeric character
         while (left < right && !isalnum(s[left])) left++;
