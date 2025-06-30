f7c7a6bf6703a8a7c8f63aa4fe9d328db112046c
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-06-30 17:58:36 +0530

    Time: 0 ms (100%), Space: 20.4 MB (62.37%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+       int jump=0;
+       int currEnd=0; // last jump
+       int fastjump=0;
+       for(int i=0;i<nums.size()-1;i++){
+        fastjump=max(fastjump,nums[i]+i);
+        // when reached end of the jump
+        if(i==currEnd){
+       jump++;
+       currEnd=fastjump;
+        }
+       }
+        return jump;
+    }
+};
