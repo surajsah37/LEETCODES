fb5b9274e42ef73a15dc123c21bfc864e3e7bd5e
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-07-04 10:56:51 +0530

    Time: 0 ms (100%), Space: 28 MB (85.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int freq = 0, candidate = 0;
-
-        // Step 1: Find the candidate
-        for (int i = 0; i < nums.size(); i++) {
-            if (freq == 0) {
-                candidate = nums[i];
+        int count=0;
+        int candidate=0;
+        for(int num:nums){
+            if(count==0){
+                candidate=num;
             }
-            if (nums[i] == candidate) {
-                freq++;
-            } else {
-                freq--;
-            }
-        }
-        // Step 2: Verify the candidate
-        int count = 0;
-        for (int num : nums) {
-            if (num == candidate) {
+            if(num==candidate){
                 count++;
             }
+            else{
+                count--;
+            }
         }
-        if (count > nums.size() / 2) {
-            return candidate;
-        }
-        // If no majority element exists (depends on problem constraints)
-        return -1;  // Or throw an error if majority is guaranteed
+        return candidate;
     }
 };
