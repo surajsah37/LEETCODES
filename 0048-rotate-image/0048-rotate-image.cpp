a17a1471b9d09b3d9c4a87bf2e84f8e866d52fb3
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-07-04 12:13:43 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.28%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -3,26 +3,9 @@ public:
     void rotate(vector<vector<int> >& matrix) {
         
     int n=matrix.size();
-    /*
-    // transpose of the matrix
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        swap(matrix[i][j],matrix[j][i]);
-    }
-        // reverese of the matrix
-        for(int i=0;i<n;i++){
-            int start=0,end=n-1;
-            while(start<end){
-                swap(matrix[i][start],matrix[i][end]);
-                start++,end--;
-            }
-        }
-    }
-    */
       // Step 1: Transpose the matrix (swap elements across the diagonal)
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) { // Ensure swapping only in upper triangle
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) { // Ensure swapping only in upper triangle
             swap(matrix[i][j], matrix[j][i]); // Swap elements to transpose
         }
     }
