a5bcd3f437a3acc19d853c6f80f4dc3288661a06
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-07-04 16:50:17 +0530

    Time: 0 ms (100%), Space: 23.6 MB (78.2%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+         int n = nums.size();
+
+        // Step 1: Find 2 potential candidates using Boyer-Moore Voting
+        int count1 = 0, count2 = 0;
+        int candidate1 = 0, candidate2 = 1; 
+
+       for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Step 2: Verify the counts of the candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            if (num == candidate2) count2++;
+        }
+
+        // Step 3: Collect valid results
+        vector<int> result;
+        if (count1 > n / 3) result.push_back(candidate1);
+        if (count2 > n / 3 && candidate2 != candidate1) result.push_back(candidate2);
+
+        return result;
+    }
+};
