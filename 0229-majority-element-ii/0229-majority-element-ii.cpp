4c38ba98a1d9a023979402e1072f1ddaff8fd38b
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-07-04 16:52:48 +0530

    Time: 0 ms (100%), Space: 23.6 MB (78.2%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,42 +2,44 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
          int n = nums.size();
-
-        // Step 1: Find 2 potential candidates using Boyer-Moore Voting
-        int count1 = 0, count2 = 0;
-        int candidate1 = 0, candidate2 = 1; 
-
-       for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
+        int ele1 = NULL;
+        int ele2 = NULL;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == ele1){
+                cnt1++;
+            }
+            else if(nums[i] == ele2){
+                cnt2++;
+            }
+            else if(cnt1 == 0){
+                ele1 = nums[i];
+                cnt1 = 1;
+            }
+            else if(cnt2 == 0){
+                ele2 = nums[i];
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
-
-        // Step 2: Verify the counts of the candidates
-        count1 = 0;
-        count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            if (num == candidate2) count2++;
+        vector<int> ans;
+        int freq1 = 0, freq2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == ele1){
+                freq1++;
+            }
+            else if(nums[i] == ele2){
+                freq2++;
+            }
         }
-
-        // Step 3: Collect valid results
-        vector<int> result;
-        if (count1 > n / 3) result.push_back(candidate1);
-        if (count2 > n / 3 && candidate2 != candidate1) result.push_back(candidate2);
-
-        return result;
+        if(freq1 > floor(n/3))
+        ans.push_back(ele1);
+        if(freq2 > floor(n/3))
+        ans.push_back(ele2);
+        return ans;
     }
 };
