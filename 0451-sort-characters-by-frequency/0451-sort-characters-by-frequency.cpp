624294b9c427962541389dd602261479f3121455
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-07-08 11:20:04 +0530

    Time: 15 ms (6.54%), Space: 10.2 MB (98.77%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
     string frequencySort(string s) {
+     int freq[128]={0};
+     for(char c:s){
+        freq[c]++;
+     }
+     //sort using the comparator
+     sort(s.begin(),s.end() ,[&](char a,char b){
+        if(freq[a]!=freq[b]) 
+        return freq[a]>freq[b];
+        return a<b;
+     });
+     return s;
+    }
+};
+
+
+
+
+
+
+
+
+
+   /*
         unordered_map<char, int> freq; // To store frequency of each character
 
     // Step 1: Count the frequency
     for (char c : s) {
         freq[c]++;
     }
-
     // Step 2: Create buckets where index = frequency
     int n = s.size();
     vector<vector<char>> buckets(n + 1); // size n+1 because frequency can be up to n
@@ -15,7 +37,6 @@ public:
     for (auto it : freq) {
         buckets[it.second].push_back(it.first); // Put character in its frequency bucket
     }
-
     // Step 3: Build the final string from high frequency to low
     string result = "";
     for (int i = n; i >= 1; i--) { // from highest possible frequency to lowest
@@ -23,8 +44,5 @@ public:
             result.append(i, c); // add character 'i' times
         }
     }
-
     return result;
-
-    }
-};
+    */
