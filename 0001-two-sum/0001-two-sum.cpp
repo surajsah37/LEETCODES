2f96026c7c9e406f74b94746c7456d7040895338
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-07-15 16:44:48 +0530

    Time: 1 ms (79.97%), Space: 15 MB (21.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-          unordered_map<int, int> seen;  // stores number and its index
-
-    for (int i = 0; i < nums.size(); i++) {
-        int current = nums[i];
-        int needed = target - current;  // what number we are looking for
-
-        // check if needed number is already in the map
-        if (seen.find(needed) != seen.end()) {
-            return {seen[needed], i};  // return indices
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int compliment=target-nums[i];
+            if(mp.find(compliment)!=mp.end()){
+                return {mp[compliment],i};
+            }
+             mp[nums[i]] = i;
         }
-
-        // store the current number with its index
-        seen[current] = i;
+        return {};
     }
-
-    return {}; // just in case, though question says one solution always exists
-}
 };
