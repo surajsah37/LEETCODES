a2b913a01594a95338982bbfdb93788e6bfe1388
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-08-13 10:04:14 +0530

    Time: 13 ms (45.19%), Space: 34.9 MB (95.45%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -4,28 +4,22 @@ public:
         int low = *max_element(weights.begin(), weights.end());
         int high = accumulate(weights.begin(), weights.end(), 0);
         int result = high;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
             int requiredDays = 1;
             int currentLoad = 0;
-
             for (int w : weights) {
                 if (currentLoad + w > mid) {
                     requiredDays++;
                     currentLoad = 0;
                 }
-                currentLoad += w;
-            }
-
+                currentLoad += w;   }
             if (requiredDays <= days) {
                 result = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
-            }
-        }
-
+            } }
         return result;
     }
 };
