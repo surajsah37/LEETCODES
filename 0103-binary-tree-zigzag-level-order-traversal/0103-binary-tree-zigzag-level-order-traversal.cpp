01bd3934611653eb410a6f8f30824a375958b1fc
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-08-14 11:34:45 +0530

    Time: 0 ms (100%), Space: 15.1 MB (76.62%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,39 +1,31 @@
-
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-         vector<vector<int>> result;
-         if(root==nullptr) return result;
-         queue<TreeNode*>q;
-         bool tarack=0;
-         q.push(root);
-           
-         while(!q.empty()){
-             int n=q.size();
-            vector<int>arr;
-            for(int i=0; i<n; i++){
-                TreeNode* node=q.front();
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<int> level(n);
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* node = q.front();
                 q.pop();
-                arr.push_back(node->val);
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-            if(!tarack){
-                result.push_back(arr);
-                tarack=!tarack;
-            }
-            else{
-                reverse(arr.begin(),arr.end());
-                 result.push_back(arr);
-                tarack=!tarack;
-            }
-         }
 
-         return result;
+                int index = leftToRight ? i : n - 1 - i;
+                level[index] = node->val;
 
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            leftToRight = !leftToRight;
+            result.push_back(level);
+        }
+        return result;
     }
 };
