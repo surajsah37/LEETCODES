edcf51659bbe9dbb8ab5d00b62c4f664e6098ec7
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-08-14 23:04:08 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.8%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
-
- void backtrack(vector<int>& nums, vector<vector<int>>& result, int index) {
-        // Base case: If index reaches end, store the current permutation
-        if (index == nums.size()) {
-            result.push_back(nums);  // Store a complete permutation
-            return;
-        }
-
-        // Try placing every number at the current 'index' position
-        for (int i = index; i < nums.size(); i++) {
-            swap(nums[i], nums[index]);       // Step 1: Fix the ith element at current index
-            backtrack(nums, result, index + 1); // Step 2: Recurse to fill the next index
-            swap(nums[i], nums[index]);       // Step 3: Backtrack (undo the change)
-        }
-    }
-
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> result;       // To store all permutations
-        backtrack(nums, result, 0);       // Start from index 0
+        vector<vector<int>>result;
+        sort(nums.begin(),nums.end());
+        do{
+            result.push_back(nums);
+        }
+        while(next_permutation(nums.begin(),nums.end()));
         return result;
     }
 };
