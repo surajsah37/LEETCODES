7e8f871f58e0e6b64037417d79f042a03c3e9f15
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-08-14 11:52:42 +0530

    Time: 3 ms (8.91%), Space: 15.2 MB (47.84%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        if (!root) return result;
+         vector<vector<int>> result;
+         if(root==nullptr) return result;
+         queue<TreeNode*>q;
+         bool leftToright=0;
+         q.push(root);
 
-        queue<TreeNode*> q;
-        q.push(root);
-        bool leftToRight = true;
-
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> level(n);
-
-            for (int i = 0; i < n; i++) {
-                TreeNode* node = q.front();
+         while(!q.empty()){
+             int n=q.size();
+            vector<int>level;
+            for(int i=0; i<n; i++){
+                TreeNode* node=q.front();
                 q.pop();
-
-                int index = leftToRight ? i : n - 1 - i;
-                level[index] = node->val;
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
+                level.push_back(node->val);
+                if(node->left){
+                    q.push(node->left);
+                }
+                if(node->right){
+                    q.push(node->right);
+                }
             }
+            if(!leftToright){
+                result.push_back(level);
+                leftToright=!leftToright;
+            }
+            else{
+                reverse(level.begin(),level.end());
+                 result.push_back(level);
+                leftToright=!leftToright;
+            }
+         }
+         return result;
 
-            leftToRight = !leftToRight;
-            result.push_back(level);
-        }
-        return result;
     }
 };
