9932d2d1d01e9e0abe170469384c4d66f701a1c1
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-08-19 16:06:45 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.21%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-     int n = nums.size(); // Number of elements is n, so range is from 0 to n.
-        int total = (n * (n + 1)) / 2;  // Sum of numbers from 0 to n
-        int sum = 0;
-
-        // Sum all elements in the array
-        for (int num : nums) {
-            sum += num;
-        }
-
-        // The missing number is the difference
-        return total - sum;
+    int n=nums.size();
+    int total=(n*(n+1)/2);
+    int sum=0;
+    for(int num:nums){
+        sum+=num;
     }
+    return total-sum;
+   }
 };
