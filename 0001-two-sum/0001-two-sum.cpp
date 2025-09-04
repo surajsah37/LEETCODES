d6ca6e73dedcb2a616263af7afd892a5855bfbe2
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-09-04 09:56:16 +0530

    Time: 4 ms (56.95%), Space: 14.7 MB (50.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mp;  // to store number -> index
-        for(int i=0; i<nums.size(); i++) {
-            int compliment = target - nums[i];
-            
-            // If compliment already exists in the map
-            if(mp.find(compliment) != mp.end()) {
-                return {mp[compliment], i};
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            int compliment=target-nums[i];
+            if(mp.find(compliment)!=mp.end()){
+                return {mp[compliment],i};
             }
-            
-            // Store the current number with its index
-            mp[nums[i]] = i;
+            mp[nums[i]]=i;
         }
-        return {}; // return empty if no solution
+         return {};
     }
 };
