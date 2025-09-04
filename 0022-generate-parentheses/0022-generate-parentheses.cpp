a6e7f23d9e281ff956b2606d9bbf8fc02bededc5
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-09-04 10:41:28 +0530

    Time: 3 ms (71.51%), Space: 16 MB (28.18%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     vector<string> result;
-
     void generate(int open, int close, string current, int n) {
-        // Base case: when the current string has n pairs
+      
         if (current.length() == 2 * n) {
             result.push_back(current);
             return;
         }
-
-        // If we can still add '(', add it
         if (open < n) {
             generate(open + 1, close, current + '(', n);
         }
-
-        // If we can add ')', add it (only if it won't break balance)
         if (close < open) {
             generate(open, close + 1, current + ')', n);
         }
     }
-
     vector<string> generateParenthesis(int n) {
         generate(0, 0, "", n);
         return result;
