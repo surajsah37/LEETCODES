05f9e2bf5dce0c5d018423b329a52fef483a1f27
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-09-15 12:40:18 +0530

    Time: 3 ms (51.9%), Space: 9.5 MB (98.93%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,50 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    if(s.empty()!=t.empty()) 
-    return false;
-    sort(s.begin(),s.end());
-    sort(t.begin(),t.end());
-    return s==t;
+        if(s.empty()!=t.empty())
+        return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /*
-         if (s.length() != t.length()) return false;  // If lengths are different, they can't be anagrams
-
-    vector<int> freq(26, 0);  // Array to store frequency of each character (size 26 for 'a' to 'z')
-
-    // Step 1: Count frequency of characters in string 's'
-    for (char c : s) {
-        freq[c - 'a']++;  // Increment count for character in 's'
-    }
-
-    // Step 2: Subtract frequency using characters in string 't'
-    for (char c : t) {
-        freq[c - 'a']--;  // Decrease count for character in 't'
-    }
-
-    // Step 3: Check if all counts are zero (if yes, s and t are anagrams)
-    for (int count : freq) {
-        if (count != 0) return false;  // If any count is nonzero, they are not anagrams
-    }
-
-    return true;  // All counts are zero, so they are anagrams
-
-    }
-    */
