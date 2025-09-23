967d52cab3cd5ec14be935a6fc7d8f747d18c34a
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-09-23 09:37:41 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
-          return n > 0 && (n & (n - 1)) == 0;
+    bool isPowerOfTwo(int n)
+    {
+    if (n <= 0) return false;       // powers of 2 are positive
+
+    return (n & (n - 1)) == 0;      // only one set bit check
+
     }
 };
