cfe4687e5a51b154a13e36d4fd169f5d6552a220
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-10-01 14:50:51 +0530

    Time: 0 ms (100%), Space: 12.1 MB (32.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -4,11 +4,10 @@ public:
         string ans="";
         sort(v.begin(),v.end());
         int n=v.size();
-     // Step 2: After sorting, the common prefix will be shared between the first and last strings
+     
         string first=v[0];
         string last=v[n-1];
         
-        // Step 3: Compare characters of first and last strings until a mismatch is found
         for(int i=0;i<min(first.size(),last.size());i++){
             if(first[i]!=last[i]){
                 return ans;
