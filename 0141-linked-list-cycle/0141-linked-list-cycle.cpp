64c239a1f2ccdedf6d8eb160253bb5b7e714a0b3
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-10-15 10:01:29 +0530

    Time: 6 ms (89.76%), Space: 11.7 MB (79.88%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -2,17 +2,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-         ListNode *slow = head;  // Slow pointer moves one step at a time
-        ListNode *fast = head;  // Fast pointer moves two steps at a time
-
-        while (fast != nullptr && fast->next != nullptr) { 
-            slow = slow->next;      // Move slow pointer by one step
-            fast = fast->next->next; // Move fast pointer by two steps
-
-            if (slow == fast) { // If they meet, a cycle is detected
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
                 return true;
             }
         }
-        return false; // If we reach the end, there is no cycle
+        return false;
     }
 };
