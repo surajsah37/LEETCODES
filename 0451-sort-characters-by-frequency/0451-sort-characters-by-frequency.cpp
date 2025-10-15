4dca3222ff1b39f5833d16af22e3b6b122e2d976
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-10-15 19:08:05 +0530

    Time: 16 ms (6.09%), Space: 10.2 MB (97.05%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -4,12 +4,13 @@ public:
         int freq[128]={0};
         for(char c:s){
             freq[c]++;
-        }
-        sort(s.begin(),s.end(),[&](char a,char b){
-            if(freq[a]==freq[b])
-            return a<b;
-             return freq[a]>freq[b];
-        });
+               }
+            sort(s.begin(),s.end(),[&](char a, char b){
+                if(freq[a]==freq[b])
+                return a<b;
+                return freq[a]>freq[b];
+            });
+     
         return s;
     }
 };
