aeb92e9a84644b90b12d8461504b82ccd7259de3
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-10-18 19:23:27 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.5%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -7,18 +7,24 @@ public:
         for (int i = 0; i < m; i++) {
             char ch1 = s[i]; // Current character from string s
             char ch2 = t[i];
-            // Check s -> t mapping     // Check if ch1 already has a mapping in mp1
-            if (mp1.count(ch1)) {
-                if (mp1[ch1] != ch2)
+
+            if(mp1.count(ch1)){
+                if(mp1[ch1]!=ch2)
                     return false;
-            } else {
-                mp1[ch1] = ch2;  }
-            // Check t -> s mapping
-            if (mp2.count(ch2)) {
-                if (mp2[ch2] != ch1)
-                    return false;
-            } else {
-                mp2[ch2] = ch1;
-            } }
+                     }
+                else{
+                    mp1[ch1]=ch2;
+                }
+                if(mp2.count(ch2)){
+                    if(mp2[ch2]!=ch1)
+                        return false;
+                     }
+                    else{
+                        mp2[ch2]=ch1;
+                    }
+               
+           
+        
+        }
         return true;
     }};
