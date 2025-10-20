42a3927c7d5e8ff396ac893397378afb37edd699
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-10-20 17:35:33 +0530

    Time: 0 ms (100%), Space: 12.4 MB (68.2%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,10 +7,11 @@ public:
             int mid=left+(right-left)/2;
 
             if(nums[mid]>nums[mid+1]){
-               right= mid;
+                right= mid;
+               
             }
             else{
-                left=mid+1;
+               left=mid+1;
             }
         }
         return left;
