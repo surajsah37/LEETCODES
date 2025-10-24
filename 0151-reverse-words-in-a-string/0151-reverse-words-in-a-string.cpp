779f9c579c017e9571e09cde10dffb5406f91a49
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2025-10-24 16:47:10 +0530

    Time: 0 ms (100%), Space: 10.4 MB (66.71%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     string reverseWords(string s) {
-         stack<string> st;  
-    string word, result; 
-    int n = s.length();
-  
-    for (int i = 0; i < n; i++) {
-        if (s[i] != ' ') {  
-            word += s[i];  
-        } 
-        else if (!word.empty()) { 
-            st.push(word); 
-            word = ""; 
+        stack<string> st;
+        string words, result;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] != ' ') {
+                words += s[i];
+            } 
+            else if (!words.empty()) {
+                st.push(words);
+                words = "";
+            }
         }
-    }
-    if (!word.empty()) {
-        st.push(word);
-    }
-    while (!st.empty()) {
-        result += st.top();  
-        st.pop(); 
-        if (!st.empty()) {
-            result += " ";  
+
+        // ✅ Push the last word (if string doesn't end with space)
+        if (!words.empty()) st.push(words);
+
+        // ✅ Build result by popping from stack (after reading full string)
+        while (!st.empty()) {
+            result += st.top();
+            st.pop();
+            if (!st.empty()) result += " ";
         }
-    }
-    return result;  
+
+        return result;
     }
 };
