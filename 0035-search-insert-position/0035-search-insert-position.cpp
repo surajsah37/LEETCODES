424a9abf9391bc0c953a70e9bec8cf12b49ed880
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2026-01-21 18:32:30 +0530

    Time: 0 ms (100%), Space: 13.5 MB (97.65%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& arr, int target) {
-       int left = 0, right = arr.size() - 1;  // Initialize left and right pointers
-    
-    while (left <= right) {  // Binary search loop
-        int mid = left + (right - left) / 2;  // Calculate middle index
-        
-        if (arr[mid] == target)  
-            return mid;  // Target found, return its index
-        
-        if (arr[mid] < target)  
-            left = mid + 1;  // Search in the right half
-        else  
-            right = mid - 1;  // Search in the left half
-    }
-    
-    return left;  // Left points to the insert position
+    int searchInsert(vector<int>& nums, int target) {
+       int left=0;
+       int right=nums.size()-1;
+       while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            left=mid+1;
+        }
+        else{
+            right=mid-1;
+        }
+       }
+       return left;
     }
 };
