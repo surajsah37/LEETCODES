abce45251a0dbc388ca65bd1da3a158b18569cf3
surajsah37 <160403852+surajsah37@users.noreply.github.com>
2026-01-28 12:55:29 +0530

    Time: 0 ms (100%), Space: 23.5 MB (96.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,39 +1,52 @@
-class Solution {
+class Solution
+ {
 public:
-    vector<int> majorityElement(vector<int>& nums) {
+    vector<int> majorityElement(vector<int>& nums) 
+    {
          int n = nums.size();
         int ele1 = NULL;
         int ele2 = NULL;
         int cnt1 = 0;
         int cnt2 = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == ele1){
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == ele1)
+            {
                 cnt1++;
             }
-            else if(nums[i] == ele2){
+            else if(nums[i] == ele2)
+            {
                 cnt2++;
             }
-            else if(cnt1 == 0){
+            else if(cnt1 == 0)
+            {
                 ele1 = nums[i];
                 cnt1 = 1;
             }
-            else if(cnt2 == 0){
+            else if(cnt2 == 0)
+            {
                 ele2 = nums[i];
                 cnt2 = 1;
             }
-            else{
+            else
+            {
                 cnt1--;
                 cnt2--;
-            }   }
-        vector<int> ans;
+            }
+            }
+        vector<int>ans;
         int freq1 = 0, freq2 = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == ele1){
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == ele1)
+            {
                 freq1++;
             }
-            else if(nums[i] == ele2){
+            else if(nums[i] == ele2)
+            {
                 freq2++;
-            }  }
+            }
+            }
         if(freq1 > floor(n/3))
         ans.push_back(ele1);
         if(freq2 > floor(n/3))
